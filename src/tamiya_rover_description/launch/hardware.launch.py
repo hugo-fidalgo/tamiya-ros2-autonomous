ae70baff337a7d455bb814e4bb7f8201fb300425
hugo-fidalgo <hugofidalgo51@gmail.com>
2026-08-30 22:58:47 +0200

    feat: created ros2 hardware launch file

---
+++ src/tamiya_rover_description/launch/hardware.launch.py
@@ -0,0 +1,34 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generte_launch_description():
+
+    # RPLidar C1 Node
+    rplidar_node = Node(
+        package='rplidar_ros',
+        executable='rplidar_composition',
+        name='rplidar_node',
+        output='screen',
+        parameters=[{
+            'serial_port': '/dev/rplidar',
+            'serial_baudrate': 460800,
+            'frame_id': 'laser_frame',
+            'inverted': False,
+            'angle_compensate': True,
+            'scan_mode': 'Standard'
+        }]
+    )
+
+    # Arduino MCU Bridge Node
+    mcu_bridge_node = Node(
+        package='rover_mcu_bridge',
+        executable='mcu_bridge_node',
+        name='mcu_bridge',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        rplidar_node,
+        mcu_bridge_node
+    ])
