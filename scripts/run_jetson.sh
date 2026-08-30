53c2cdcd2bc8eb664a2ef2db9c0e2d0b7637d9de
hugo-fidalgo <hugofidalgo51@gmail.com>
2026-08-30 22:03:44 +0200

    feat: added launch script for on-board-computer

---
+++ scripts/run_jetson.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]})/.." && pwd)"
+cd "$REPO_ROOT"
+
+CONTAINER_USER="dev"
+CONTAINER_WORKSPACE="/home/$CONTAINER_USER/robot_ws"
+
+echo "[INFO] Building Embedded Software Environmen (OBC)..."
+# Target the 'obc' stage defined in the Dockerfile
+docker build --target obc -t polemarchus-obc .
+
+DOCKER_ARGS=(
+    -it
+    --rm
+    --name embedded-environment
+    --user "$CONTAINER_USER"
+    --network=host
+    --ipc-host
+    -v "$REPO_ROOT:$CONTAINER_WORKSPACE:rw"
+    --group-add dialout
+)
+
+if [ -e /dev/arduino_mcu ]; then
+    echo "[INFO] Hardware detected: Mounting Arduino MCU."
+    DOCKER_ARGS+=("--device=/dev/arduiono_mcu:/dev/arduino_mcu")
+fi
+
+if [ -e /dev/rplidar ]; then
+    echo "[INFO] Hardware detected: Mountin RPLidar."
+    DOCKER_ARGS+=("--device=/dev/rplidar:/dev/rplidar")
+fi
+
+echo "[INFO] Booting Embedded OBC Container..."
+docker run "${DOCKER_ARGS[@]}" polemarchus-obs:latest /bin/bash
